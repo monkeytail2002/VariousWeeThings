ad2b193ebf95c6ce50adcb2adc8d631d385a0a1b
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2022-05-26 17:22:11 +0100

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -1,9 +1,11 @@
 # Simple scipt to find variations of a word.
-#Version 1.6
+#Version 1.7
 #Thanks to Yiab for the help making this far more efficient.
 
-#import permutations module from itertools library
+#import permutations module from itertools library and csv library
 from itertools import product
+import csv
+
 
 #Set string to user input with warning that word has to be lowercase.
 s = input("Warning!  Word must be lower case.  Enter word: ")
@@ -13,4 +15,15 @@ letters = [{ a.upper(), a.lower() } for a in s]
 #provides the answer in a list
 answer = list(map(''.join,product(*letters)))
 
-print(answer)
+#set header in csv
+header=['Permutations']
+#set data in csv
+data=[answer]
+
+#open and write to csv
+with open('permutations.csv', 'w') as f:
+    #set csv writer
+    writer = csv.writer(f)
+    #write input for csv
+    writer.writerow(header)   
+    writer.writerow(data)
